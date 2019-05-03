faa21619dbf2e7e3ae2430c94d0ce50a5b0b57e0
Marcelo Morais <marcelodotmorais@gmail.com>
2019-05-03 00:15:49 -0300

    alterada localização de bibliotecas

--- lab-2-keil/main.c
+++ lab-2-keil/main.c
@@ -12,54 +12,34 @@
 
 #include "utils/uartstdio.h"
 
-// Medida máxima para BPL 24 MHz: 1,455MHz
-// Medida máxima para BPL 120MHz: 7,300MHz
-
-#define MAX_COUNT 2999950 //2999951
-
 #define CLOCK 120000000
-
-#if CLOCK == 120000000
-	#define TIME_BASE_MAX MAX_COUNT * 5
-#else
-	#define TIME_BASE_MAX MAX_COUNT
-#endif
-
-extern uint32_t frequencyMeasure(int32_t);
 
 bool khzScale = false;
 
 int main(void)
 {
-	uint32_t ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
-	                                            SYSCTL_OSC_MAIN |
-	                                            SYSCTL_USE_PLL |
-	                                            SYSCTL_CFG_VCO_480),
-	                                           CLOCK); // PLL em 24MHz
+	uint32_t ui32SysClock = SysCtlClockFreqSet(
+		(SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480),
+		CLOCK);                            // PLL em 24MHz
 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0))
-		;
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0));
 
 	TimerConfigure(TIMER0_BASE, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_ONE_SHOT | TIMER_CFG_B_CAP_COUNT));
 
 	// Inicialização de GPIO
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
-	while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
-	{
-	}
-	GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+	while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION));
+
 	GPIOPinTypeGPIOInput(GPIO_PORTN_BASE, GPIO_PIN_1);
 
 	// Inicialização da UART
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-	while (!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0))
-	{ 
-	}
+	while (!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0));
+
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA))
-	{
-	}
+	while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA));
+
 	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 	GPIOPinConfigure(GPIO_PA0_U0RX);
 	GPIOPinConfigure(GPIO_PA1_U0TX);
@@ -67,19 +47,11 @@ int main(void)
 	UARTEchoSet(false);
 
 	uint32_t frequencyCounter = 0;
-	UARTprintf("Laboratorio 1 - Frequencimetro\n");
+	UARTprintf("Laboratorio 2 - Frequencimetro\n");
+
 	while (1)
 	{
-		while((GPIOPinRead(GPIO_PORTN_BASE, GPIO_PIN_1) & GPIO_PIN_1) == GPIO_PIN_1)
-		{
-		}
-		while((GPIOPinRead(GPIO_PORTN_BASE, GPIO_PIN_1) & GPIO_PIN_1) != GPIO_PIN_1)
-		{
-		}
-		frequencyCounter = frequencyMeasure(khzScale ? TIME_BASE_MAX / 10 : TIME_BASE_MAX);
-
 		UARTprintf("%i\n", frequencyCounter / (khzScale ? 200 : 2));
-		//	UARTprintf(khzScale ? "KHz\n" : "Hz\n");
 
 		uint8_t bytesAvailable = UARTRxBytesAvail();
 		if (bytesAvailable > 0)
