a880eee0e3813153014256554aa388666f0a2774
Bhenck <brunohscolombo@hotmail.com>
2019-06-18 12:41:04 -0300

    UART echo funcionando para strings terminadas com \r

--- lab-3-keil/main.c
+++ lab-3-keil/main.c
@@ -18,12 +18,37 @@
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 
-bool flag = false;
+osThreadId_t threadEncoderId, threadDecoderId;
+char buffer[10] = "Init\n\0";
  
  void UART0_Handler(void){
 	 uint32_t ui32Status = UARTIntStatus(UART0_BASE, true);
 	 UARTIntClear(UART0_BASE, ui32Status);
-	 flag = 1;
+	 osThreadFlagsSet(threadDecoderId, 0x0001);
+ }
+ 
+ void threadEncoder(void *arg){
+	while(true){
+		osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);
+		for(uint8_t i = 0; buffer[i] != '\0'; i++){
+			UARTCharPutNonBlocking(UART0_BASE, buffer[i]);
+		}
+	}
+ }
+ 
+ void threadDecoder(void *arg){
+	 while(true){
+			osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);
+			uint8_t i;
+			for(i = 0; i<9; i++){
+				buffer[i] = UARTCharGet(UART0_BASE);
+				if(buffer[i] == '\r'){
+					break;
+				}		
+			}
+			buffer[++i] = '\0';
+			osThreadFlagsSet(threadEncoderId, 0x0001);
+	 }
  }
  
 /*----------------------------------------------------------------------------
@@ -35,46 +60,33 @@ void app_main (void *argument) {
 	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 	GPIOPinConfigure(GPIO_PA0_U0RX);
 	GPIOPinConfigure(GPIO_PA1_U0TX);
-	
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
 	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0));
-	UARTConfigSetExpClk(UART0_BASE, SystemCoreClock, 57600, (UART_CONFIG_PAR_NONE | UART_CONFIG_STOP_ONE | UART_CONFIG_WLEN_8));
-//	UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8, UART_FIFO_RX1_8);
+	UARTConfigSetExpClk(UART0_BASE, SystemCoreClock, 115200, (UART_CONFIG_PAR_NONE | UART_CONFIG_STOP_ONE | UART_CONFIG_WLEN_8));
+	UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8, UART_FIFO_RX1_8);
 	UARTIntDisable(UART0_BASE, 0xFFFFFFFF);
 	UARTIntEnable(UART0_BASE, UART_INT_RX);
 	IntEnable(INT_UART0);
 	UARTEnable(UART0_BASE);
-	UARTFIFODisable(UART0_BASE);
-	char buffer[10] = "Init\n\0";
-	uint8_t i = 0;
-	while(buffer[i] != '\0'){
-		UARTCharPut(UART0_BASE, buffer[i++]);
+	
+	for(uint8_t i = 0; buffer[i] != '\0'; i++){
+		UARTCharPut(UART0_BASE, buffer[i]);
 	}
 	
-  for (;;) {
-		if(flag == true){
-			uint8_t i = 0;
-			while(UARTCharsAvail(UART0_BASE) && i<8){
-				buffer[i++] = UARTCharGetNonBlocking(UART0_BASE);
-			}
-			buffer[i] = '\0';
-			i = 0;
-			while(buffer[i] != '\0'){
-				UARTCharPut(UART0_BASE, buffer[i++]);
-			}
-			flag = false;
-		}
+	threadEncoderId = osThreadNew(threadEncoder, NULL, NULL);
+	threadDecoderId = osThreadNew(threadDecoder, NULL, NULL);
+	
+  while(true){
+		osDelay(osWaitForever);
 	}
 }
  
 int main (void) {
- 
-  // System Initialization
   SystemCoreClockUpdate();
-  // ...
-	
-  osKernelInitialize();                 // Initialize CMSIS-RTOS
-  osThreadNew(app_main, NULL, NULL);    // Create application main thread
-  osKernelStart();                      // Start thread execution
-  for (;;) {}
+  osKernelInitialize();
+  osThreadNew(app_main, NULL, NULL);
+  if(osKernelGetState() == osKernelReady){
+		osKernelStart();
+	}
+  while(true);
 }
