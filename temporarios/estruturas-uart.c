742d6f5422c5f2f3ee21389a1a335f88608e1c5d
Marcelo <marcelodotmorais@gmail.com>
2019-06-27 19:07:04 -0300

    Adicionado código inicial de estruturas de decodificação de UART

---
+++ temporarios/estruturas-uart.c
@@ -0,0 +1,49 @@
+typedef struct
+{
+	TipoEntrada tipo;
+	char dados[2];
+} EventoEntrada;
+
+typedef enum
+{
+	PASSOU_POR_ANDAR = 0,
+	BOTAO_INTERNO,
+	BOTAO_EXTERNO,
+	PORTAS
+} TipoEntrada;
+
+typedef enum
+{
+	ABERTAS = 0,
+	FECHADAS
+} EstadoPortas;
+
+typedef enum
+{
+	SUBIDA = 0,
+	DESCIDA
+} DirecaoBotao;
+
+// ---------------------------
+
+typedef struct
+{
+	TipoSaida tipo;
+	char dados[2];
+} EventoSaida;
+
+typedef enum
+{
+	MOVIMENTO = 0,
+	LUZES
+} TipoSaida;
+
+typedef enum
+{
+	INICIALIZA = 0,
+	ABRE_PORTAS,
+	FECHA_PORTAS,
+	SOBE,
+	DESCE,
+	PARA
+} TipoMovimento;
