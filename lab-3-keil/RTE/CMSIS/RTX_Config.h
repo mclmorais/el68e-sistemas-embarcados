306d9a60604285b63ce2d1c5a06d47b7727596ba
Bhenck <brunohscolombo@hotmail.com>
2019-07-06 03:46:17 -0300

    Pendencias externas e internas de subida funcionando corretamente - bug na descida e transicao descida para subida

--- lab-3-keil/RTE/CMSIS/RTX_Config.h
+++ lab-3-keil/RTE/CMSIS/RTX_Config.h
@@ -44,7 +44,7 @@
 //   <i> Defines the combined global dynamic memory size.
 //   <i> Default: 4096
 #ifndef OS_DYNAMIC_MEM_SIZE
-#define OS_DYNAMIC_MEM_SIZE         4096
+#define OS_DYNAMIC_MEM_SIZE         2*4096
 #endif
  
 //   <o>Kernel Tick Frequency [Hz] <1-1000000>
@@ -124,7 +124,7 @@
 //   <i> Defines stack size for threads with zero stack size specified.
 //   <i> Default: 256
 #ifndef OS_STACK_SIZE
-#define OS_STACK_SIZE               256
+#define OS_STACK_SIZE               2*256
 #endif
  
 //   <o>Idle Thread Stack size [bytes] <72-1073741824:8>
---
+++ lab-3-keil/estruturas-elevador.h
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+enum EstadoPortas;
+
+typedef enum EstadoElevador
+{
+	INICIALIZANDO = 0,
+	PRONTO,
+	FECHANDO_PORTAS,
+	ABRINDO_PORTAS,
+	DESCENDO,
+	SUBINDO,
+	AGUARDANDO
+} EstadoElevador;
+
+typedef struct StructElevador
+{
+	EstadoElevador estadoAtual;
+  EstadoElevador estadoAnterior;
+  uint8_t andarAtual;
+  uint8_t andarAlvo;
+	uint16_t pendentesSubida;
+	uint16_t pendentesDescida;
+  uint16_t pendentesInterno;
+	bool mensagemEnviada;
+} StructElevador;
